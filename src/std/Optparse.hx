188f77511ce5ac279e2ef964eb35a5efe95bd0f5
Sun <gsun@coriant.com>
2020-10-27 13:35:55 +0800

    Create Optparse.hx

---
+++ src/std/Optparse.hx
@@ -0,0 +1,19 @@
+package std;
+
+@:pythonImport("optparse", "Values") extern class Values { }
+@:pythonImport("optparse", "Option") extern class Option { }
+
+@:pythonImport("optparse", "OptionGroup") 
+extern class OptGroup {
+    public function add_options(option_list:Array<Option>):Void;
+}
+
+@:pythonImport("optparse", "OptParser") 
+extern class OptParser {
+    public function add_option_group(?args:String, ?kwargs:python.KwArgs<Dynamic>):OptGroup;
+}
+
+@:pythonImport("optparse") 
+extern class Optparse {
+    static public function make_option(?opts:String, ?attrs:python.KwArgs<Dynamic>):Option;
+}
